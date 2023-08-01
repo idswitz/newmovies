6c97a2730febbd7cfce7a3214df383f851972c19
idswitz <idrisolamilekan434@gmail.com>
2023-08-01 08:35:25 -0700

    correction

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
 import { GlobalStyle } from './GlobalStyle';
-import Thumb from './component/thumb';
-import Grid from './component/grid';
-import Banner from './component/banner';
 import Header from './component/header';
-import SearchBar from './component/searchbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './component/pages/home';
 import Movie from './component/pages/movie';
